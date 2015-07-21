1f3de3e609aa7a5682a9dae9d45e61418a717307
sunitamis <sunitamistry8@gmail.com>
2015-07-22 00:00:33 +0530

    application folder add

---
+++ application/views/case_form.php
@@ -0,0 +1,70 @@
+<div class="normalheader transition animated fadeIn small-header">
+    <div class="hpanel">
+        <div class="panel-body">
+            <a class="small-header-action" href="#">
+                <div class="clip-header">
+                    <i class="fa fa-arrow-down"></i>
+                </div>
+            </a>
+
+            <!--<div id="hbreadcrumb" class="pull-right">
+                <ol class="hbreadcrumb breadcrumb">
+                    <li><a href="index-2.html">Dashboard</a></li>
+                    <li>
+                        <span>Interface</span>
+                    </li>
+                    <li class="active">
+                        <span>Panels design</span>
+                    </li>
+                </ol>
+            </div>-->
+            <h2 class="font-light m-b-xs">
+               Add New Inquiry
+            </h2>
+        </div>
+    </div>
+</div>
+
+<div class="content animate-panel">
+
+ <div class="col-lg-8">
+        <div class="hpanel hblue">
+            <div class="panel-heading hbuilt">
+                <div class="panel-tools">
+                    <a class="showhide"><i class="fa fa-chevron-up"></i></a>
+                    <a class="closebox"><i class="fa fa-times"></i></a>
+                </div>
+                 Add New Inquiry
+            </div>
+            <div class="panel-body">
+                <form method="get" class="form-horizontal">
+                    <div class="form-group col-lg-12 ">
+                        <input type="text" class="form-control" placeholder = "Type Customer Name/Code/Email/Phone"> 
+                        <span class="input-group-btn"> <button type="button" class="btn btn-primary"> + </button> </span>
+                    </div>
+                
+                <div class="form-group col-lg-6">
+                                            <label>Password</label>
+                                            <input type="password" value="" id="" class="form-control" name="" placeholder="******">
+                                        </div>
+
+                <div class="form-group col-lg-6">
+                                            <label>Password</label>
+                                            <input type="password" value="" id="" class="form-control" name="" placeholder="******">
+                                        </div>
+
+                <div class="form-group col-lg-6">
+                                            <label>Password</label>
+                                            <input type="password" value="" id="" class="form-control" name="" placeholder="******">
+                                        </div>
+
+                <div class="form-group col-lg-6">
+                                            <label>Password</label>
+                                            <input type="password" value="" id="" class="form-control" name="" placeholder="******">
+                                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</div>
